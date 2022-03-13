161638a5099376bee4e87f3a4decc06dc518dfcc
juanpa <jpblanco@qcompusoft.com>
2022-03-13 21:26:30 +0100

    clean

--- deletenamespaces.sh
+++ deletenamespaces.sh
@@ -2,7 +2,6 @@ push(){
     curl -k -H "Content-Type: application/json" -X PUT --data-binary @$i.json http://127.0.0.1:8001/api/v1/namespaces/$i/finalize
 }
 reemplaceTerminating(){
-    echo "-------reemplaceTerminating------------"
     findline
     if [ $number == '' ]
     then
@@ -15,11 +14,9 @@ reemplaceTerminating(){
       sed -i "${number},${fin}d" $i.json
       push
     fi
-    echo "------- end reemplaceTerminating------------"
 }
 
 findline(){
-    echo "-------findline------------"
     number=`grep -m 1 -n "finalizers" $i.json  | awk -F ":" {'print $1'}`
     return $number
 }
@@ -36,14 +33,3 @@ do
 done
  
 echo "--------------End----------"
-
-
-
-# foreach i ( ns )
-#     echo "Working $i filename ..."
-# end
-
-
-# kubectl get namespace p-h97kt -o json > tmp.json
-
-# curl -k -H "Content-Type: application/json" -X PUT --data-binary @tmp.json http://127.0.0.1:8001/api/v1/namespaces/p-h97kt/finalize
